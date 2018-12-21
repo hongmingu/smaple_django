16d0659acc369c6be9a96cfd9996f8a03799d6fd
hongmingu <gmh397@nate.com>
2018-12-21 15:47:31 +0900

    Update models.py

--- testapp/models.py
+++ testapp/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class TestModel(models.Model):
-    number = models.IntegerField(default=0)
-
-    def __str__(self):
-        return "pk: %s, number: %s" % (self.pk, self.number)
